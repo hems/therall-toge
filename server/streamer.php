9d1aa51290985f9ef4bd542c21d0654a434e1512
hems <hems.inlet@gmail.com>
2014-09-02 04:11:03 +0100

    hiding client id

---
+++ server/streamer.php
@@ -0,0 +1,13 @@
+<?php
+
+$fp = fopen('http://api.soundcloud.com/tracks/' . $_REQUEST['track_id'] . '/stream?client_id=[YOUR_CLIENT_ID]', 'rb');
+
+header("Content-Type: audio/mpeg");
+header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE");
+header("Access-Control-Allow-Headers: Accept, Authorization, Content-Type, Origin");
+header("Access-Control-Allow-Origin: http://hems.github.io");
+header("Access-Control-Allow-Origin: https://hems.github.io");
+header("Access-Control-Allow-Origin: http://localhost:3000");
+fpassthru( $fp );
+
+exit();
