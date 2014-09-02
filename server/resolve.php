ad725f41aa9e2e46e417508af62eb6850ad2a338
hems <hems.inlet@gmail.com>
2014-09-02 04:21:48 +0100

    updating server code

--- server/resolve.php
+++ server/resolve.php
@@ -4,7 +4,7 @@
 	header("Access-Control-Allow-Headers: Accept, Authorization, Content-Type, Origin");
 	header("Access-Control-Allow-Origin: http://hems.github.io");
 	header("Access-Control-Allow-Origin: https://hems.github.io");
-	header("Access-Control-Allow-Origin: http://localhost:3000");
+	// header("Access-Control-Allow-Origin: http://localhost:3000");
 
 	// get user URL from Wordpress custom field
 	$sc_url = $_REQUEST[ 'track_url'];
@@ -12,4 +12,4 @@
 	// if $sc_url is not empty, do
 	if ( empty( $sc_url ) ) exit();
 
-	echo file_get_contents( 'https://api.soundcloud.com/resolve.json?url=' . $sc_url . '&client_id=[YOUR_CLIENT_ID]' );
+	echo file_get_contents( 'https://api.soundcloud.com/resolve.json?url='.$sc_url.'&client_id=[YOUR_CLIENT_ID]' );
